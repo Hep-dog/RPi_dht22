c1556ac3f057a23676da78a20eb08da69a569f33
Jiyizi <shenpx@ihep.ac.cn>
2020-01-13 16:17:16 +0800

    Add the scripts for connect clients in different local networks

--- Sync/trans.sh
+++ Sync/trans.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+remote_ip=192.168.43.84
+
 /usr/bin/inotifywait -mrq -e modify,delete,create,attrib,move /home/pi/Work/RPi_dht22/Collect/data/ | while read events
 do
-	sudo rsync -av --delete --password-file=/etc/rsyncd.secrets /home/pi/Work/RPi_dht22/Collect/data/ pi@192.168.137.60::testsource
+	sudo rsync -av --password-file=/etc/rsyncd.secrets /home/pi/Work/RPi_dht22/Collect/data/ pi@${remote_ip}::dht11
 	sudo echo "`date +'%F %T'` have $events" >> /tmp/rsync.log 2>&1
 done
-
